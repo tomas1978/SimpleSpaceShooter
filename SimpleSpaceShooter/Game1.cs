7b35a71461fda436a9f80c16b3041edddb5ebcd6
Tomas Nilsson <tomas.nilsson78@gmail.com>
2021-03-23 09:10:03 +0100

    Add project files.

---
+++ SimpleSpaceShooter/Game1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SimpleSpaceShooter
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D player;
+        Vector2 playerPos = new Vector2(300, 350);
+
+        Texture2D enemy;
+        Vector2 enemyPos = new Vector2(300, 100);
+        int enemyDirection = -1;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            player = Content.Load<Texture2D>("spaceship96");
+            enemy = Content.Load<Texture2D>("alien");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            KeyboardState kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.Left))
+                playerPos.X--;
+            if (kstate.IsKeyDown(Keys.Right))
+                playerPos.X++;
+
+            if (enemyPos.X > 700 || enemyPos.X < 0)
+                enemyDirection *= -1;
+            enemyPos.X += enemyDirection;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(player, playerPos, Color.White);
+
+            _spriteBatch.Draw(enemy, enemyPos, Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
